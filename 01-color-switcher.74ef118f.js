ec170ee083182dc93528a6bcb619d5545f936cf5
sammy-acosta19 <115603325+sammy-acosta19@users.noreply.github.com>
2023-10-06 02:06:25 +0000

    Deploying to gh-pages from @ sammy-acosta19/goit-js-hw-09@130940620616fe1838e0d174928567ab8c9c24d1 🚀

---
+++ 01-color-switcher.74ef118f.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let a=null;function d(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}t.addEventListener("click",(()=>{a||(a=setInterval(d,1e3),t.disabled=!0)})),e.addEventListener("click",(()=>{clearInterval(a),a=null,t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.74ef118f.js.map
